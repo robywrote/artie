9bb44575a8020fc44d40818db4736df0fb10a79c
Sean Roby <robywrote@gmail.com>
2022-07-06 12:39:38 -0500

    add mousedown and mouseup events

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 const clearButton = document.querySelector("#clear-grid");
 clearButton.addEventListener("click", clearGrid);
 
-const squares = 32768;
-const rowsize = 256;
+const squares = 8192;
+const rowSize = 128;
+const squareSize = "0.5rem";
 const gridContainer = document.querySelector("#grid-container");
-document.documentElement.style.setProperty("--rowsize", rowsize);
+document.documentElement.style.setProperty("--rowsize", rowSize);
+document.documentElement.style.setProperty("--squaresize", squareSize);
 const gridColor = "darkslateblue";
 let currentColor = gridColor;
+let isPainting = false;
 
 const palette = document.querySelector("#palette");
 const paletteColors = ["lightcoral", "skyblue", "moccasin", "palegreen"];
@@ -28,7 +31,9 @@ function buildGrid(squares) {
   for (let i = 0; i < squares; i++) {
     const square = document.createElement("div");
     square.style.backgroundColor = gridColor;
-    square.addEventListener("mouseover", paint);
+    square.addEventListener("mouseover", checkPainting);
+    square.addEventListener("mousedown", startPainting);
+    square.addEventListener("mouseup", stopPainting);
     gridContainer.appendChild(square);
   }
 }
@@ -46,6 +51,21 @@ function setCurrentColor(e) {
   const swatch = e.target;
   const color = swatch.style.backgroundColor;
   currentColor = color;
+}
+
+function startPainting(e) {
+  isPainting = true;
+  paint(e);
+}
+
+function checkPainting(e) {
+  if (isPainting) {
+    paint(e);
+  }
+}
+
+function stopPainting(e) {
+  isPainting = false;
 }
 
 function paint(e) {
